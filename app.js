5d115f1cf87cb9a02667023c94e2ef9fa4940248
leducgiachoang <69897727+leducgiachoang@users.noreply.github.com>
2021-08-29 10:25:41 +0700

    ok fine

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ let messenge_error = document.querySelector('#messenge_error');
 buttom.onclick = function(){
     loading_send.classList.add('fa-spinner');
     if(address.value == ''){
-        
-        alert('Vui lòng điền địa chỉ');
+        messenge_error.style.display = 'block';
+        messenge_error.innerHTML = 'Địa chỉ không được để trống. Vui lòng thử lại!';
         loading_send.classList.remove('fa-spinner');
     }else{
         fetch('https://api.openweathermap.org/data/2.5/weather?q='+address.value+'&appid='+AppApi+'&lang=vi&units=metric')
@@ -87,17 +87,11 @@ buttom_left.onclick = function(){
 
 function convertTime(time){
     let unix_timestamp = time
-// Create a new JavaScript Date object based on the timestamp
-// multiplied by 1000 so that the argument is in milliseconds, not seconds.
 var date = new Date(unix_timestamp * 1000);
-// Hours part from the timestamp
 var hours = date.getHours();
-// Minutes part from the timestamp
 var minutes = "0" + date.getMinutes();
-// Seconds part from the timestamp
 var seconds = "0" + date.getSeconds();
 
-// Will display time in 10:30:23 format
 var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2) + 's';
 
 return formattedTime;
