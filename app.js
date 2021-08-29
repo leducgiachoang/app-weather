6dcbc65d8630ad4d332b6e05ef8d46c1a3a4d530
leducgiachoang <69897727+leducgiachoang@users.noreply.github.com>
2021-08-29 09:32:19 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -6,16 +6,29 @@ let box_caner = document.querySelector('.box_caner');
 let box_show_weather = document.querySelector('.show_weather');
 let name_location = document.querySelector('#name_location');
 let icon_weather = document.querySelector('#icon_weather');
+let buttom_left = document.querySelector('#buttom_left');
+let loading_click = document.getElementById('loading_click');
+let loading_send = document.querySelector('#loading_send');
+let messenge_error = document.querySelector('#messenge_error');
 
 buttom.onclick = function(){
-    fetch('https://api.openweathermap.org/data/2.5/weather?q='+address.value+'&appid='+AppApi+'&lang=vi&units=metric')
-    .then(response => response.json())
-    .then(data => {
-        Showweather(data)
-    });
+    loading_send.classList.add('fa-spinner');
+    if(address.value == ''){
+        
+        alert('Vui lòng điền địa chỉ');
+        loading_send.classList.remove('fa-spinner');
+    }else{
+        fetch('https://api.openweathermap.org/data/2.5/weather?q='+address.value+'&appid='+AppApi+'&lang=vi&units=metric')
+        .then(response => response.json())
+        .then(data => {
+            Showweather(data)
+        });
+    }
+    
 }
 
 locationj.onclick = function(){
+    loading_click.classList.add('fa-spinner');
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition((position) => {
             const lat = position.coords.latitude;
@@ -30,32 +43,46 @@ locationj.onclick = function(){
 }
 
 function Showweather(data){
-    box_caner.style.display = 'none';
-    box_show_weather.style.display = 'block';
-    name_location.innerHTML = data.name;
+    loading_click.classList.remove('fa-spinner');
+    loading_send.classList.remove('fa-spinner');
+    messenge_error.style.display = 'none';
+    console.log(data)
+    if(data.cod >= 400){
+        messenge_error.style.display = 'block';
+        messenge_error.innerHTML = 'Địa chỉ không tồn tại. Vui lòng thử lại!';
+        
+    }else{
+        box_caner.style.display = 'none';
+        box_show_weather.style.display = 'block';
+        name_location.innerHTML = data.name;
 
-    let weather = data.weather[0];
-    let main = data.main;
-    let sys = data.sys;
-    let clouds = data.clouds;
-    let wind = data.wind;
+        let weather = data.weather[0];
+        let main = data.main;
+        let sys = data.sys;
+        let clouds = data.clouds;
+        let wind = data.wind;
 
-    icon_weather.src = 'images/'+weather.icon+'@2x.png';
-    document.querySelector('#description').innerHTML = weather.description;
+        icon_weather.src = 'images/'+weather.icon+'@2x.png';
+        document.querySelector('#description').innerHTML = weather.description;
 
-    document.querySelector('#temp').innerHTML = main.temp;
-    document.querySelector('#humidity').innerHTML = main.humidity;
+        document.querySelector('#temp').innerHTML = main.temp;
+        document.querySelector('#humidity').innerHTML = main.humidity;
 
-    document.querySelector('#sunrise').innerHTML = convertTime(sys.sunrise);
-    document.querySelector('#sunset').innerHTML = convertTime(sys.sunset);
+        document.querySelector('#sunrise').innerHTML = convertTime(sys.sunrise);
+        document.querySelector('#sunset').innerHTML = convertTime(sys.sunset);
 
-    document.querySelector('#speed').innerHTML = wind.speed;
-    document.querySelector('#deg').innerHTML = wind.deg;
-    document.querySelector('#gust').innerHTML = wind.gust;
+        document.querySelector('#speed').innerHTML = wind.speed;
+        document.querySelector('#deg').innerHTML = wind.deg;
+        document.querySelector('#gust').innerHTML = wind.gust;
 
-    document.querySelector('#all').innerHTML = clouds.all;
+        document.querySelector('#all').innerHTML = clouds.all;
+    }
 
-    console.log(data);
+}
+
+buttom_left.onclick = function(){
+    box_caner.style.display = 'block';
+    box_show_weather.style.display = 'none';
 }
 
 function convertTime(time){
